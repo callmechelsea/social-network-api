67dbb6e1180939b083428bf246b7ed0d7a333976
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-13 13:26:22 -0700

    tested more routes in insomnia

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -16,9 +16,22 @@ router.get("/:userId", async (req, res) => {
 });
 
 // POST a new user:
+router.post("/", async (req, res) => {
+    const newUser = await User.create(req.body);
+    res.status(200).json(newUser);
+});
 
 // PUT to update a user by its _id
+router.put("/:userId", async (req, res) => {
+    const updateUser = await User.findByIdAndUpdate
+    (req.params.userId, req.body, {new: true});
+    res.status(200).json(updateUser);
+});
 
 // DELETE to remove user by its _id
+router.delete("/:userId", async (req, res) => {
+    const deleteUser = await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User deleted");
+});
 
 module.exports = router;
