3f8dbc44a47df8a32ccc0b2d8b641dbcaefcac3f
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-13 14:30:39 -0700

    added friend count

--- models/user.js
+++ models/user.js
@@ -1,19 +1,27 @@
 const { model, Schema, Types } = require("mongoose");
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^.+@(?:[\w-]+\.)+\w+$/, "Must contain a valid email address!"],
+    },
+    thoughts: [{ type: Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: Types.ObjectId, ref: "User" }],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/^.+@(?:[\w-]+\.)+\w+$/, "Must contain a valid email address!"],
-  },
-  thoughts: [{ type: Types.ObjectId, ref: "Thought" }],
-  friends: [{ type: Types.ObjectId, ref: "User" }],
+
+  { toJSON: { virtuals: true } }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
 const User = model("User", userSchema);
