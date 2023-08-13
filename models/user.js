d70743b0070b27181dddb18996ffa9f1bf6253a6
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-13 11:54:35 -0700

    added user schema

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const { model, Schema } = require("mongoose");
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^.+@(?:[\w-]+\.)+\w+$/, "Must contain a valid email address!"],
+  },
+  thoughts: [{ type: Types.ObjectId, ref: "Thought" }],
+  friends: [{ type: Types.ObjectId, ref: "User" }],
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
