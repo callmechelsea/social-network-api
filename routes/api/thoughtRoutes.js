c1df603f0b0a688a0755ffe8498078e958fdf817
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-13 14:09:58 -0700

    finished and tested reactions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,41 +4,60 @@ const { User, Thought } = require("../../models");
 
 // GET to get all thoughts
 router.get("/", async (req, res) => {
-    const thoughts = await Thought.find();
-    res.status(200).json(thoughts);
+  const thoughts = await Thought.find();
+  res.status(200).json(thoughts);
 });
 
 // GET to get a single thought by its _id
 router.get("/:thoughtId", async (req, res) => {
-    const oneThought = await Thought.findById(req.params.thoughtId);
-    res.status(200).json(oneThought);
+  const oneThought = await Thought.findById(req.params.thoughtId);
+  res.status(200).json(oneThought);
 });
 
 // POST to create a new thought
 router.post("/", async (req, res) => {
   const newThought = await Thought.create(req.body);
 
-  const updateUser = await User.findByIdAndUpdate(
-    req.body.userId,
-    { $push: { thoughts: newThought } },
-    );
-    res.status(200).json(newThought);
+  const updateUser = await User.findByIdAndUpdate(req.body.userId, {
+    $push: { thoughts: newThought },
+  });
+  res.status(200).json(newThought);
 });
 
 // PUT to update a thought by its _id
 router.put("/:thoughtId", async (req, res) => {
-    const updateThought = await Thought.findByIdAndUpdate
-    (req.params.thoughtId, req.body, {new: true});
-    res.status(200).json(updateThought);
+  const updateThought = await Thought.findByIdAndUpdate(
+    req.params.thoughtId,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updateThought);
 });
 
 // DELETE to remove a thought by its _id
 router.delete("/:thoughtId", async (req, res) => {
-    const deleteThought = await Thought.findByIdAndDelete(req.params.thoughtId);
-    res.status(200).json("Thought deleted");
+  const deleteThought = await Thought.findByIdAndDelete(req.params.thoughtId);
+  res.status(200).json("Thought deleted");
 });
 
 // POST to create a reaction stored in a single thought's reactions array field
+router.post("/:thoughtId/reactions", async (req, res) => {
+  const newReaction = await Thought.findByIdAndUpdate(
+    req.params.thoughtId,
+    { $push: { reactions: req.body } },
+    { new: true }
+  );
+  res.status(200).json(newReaction);
+});
 
 // DELETE to pull and remove a reaction by the reaction's reactionId value
+router.delete("/:thoughtId/reactions/:reactionId", async (req, res) => {
+  const deleteReaction = await Thought.findByIdAndUpdate(
+    req.params.thoughtId,
+    { $pull: { reactions: { reactionId: req.params.reactionId } } },
+    { new: true }
+  );
+  res.status(200).json("Reaction deleted");
+});
+
 module.exports = router;
