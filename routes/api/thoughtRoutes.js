1835235a4783f21d2276930514b628f59fb8425e
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-13 12:21:09 -0700

    added pseudo code

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,14 @@
+// GET to get all thoughts
+
+// GET to get a single thought by its _id
+
+// POST to create a new thought 
+
+// PUT to update a thought by its _id
+
+// DELETE to remove a thought by its _id
+
+// POST to create a reaction stored in a single thought's reactions array field
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
+
