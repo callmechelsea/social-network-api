20f8bd643f9ce8695b419924332e36085e00a7c0
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-13 13:17:59 -0700

    tested by userID

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,14 +1,19 @@
-const router = require('express').Router();
-const { Types } = require('mongoose');
-const { User, Thought } = require('../../models');
+const router = require("express").Router();
+const { Types } = require("mongoose");
+const { User, Thought } = require("../../models");
+const { request } = require("http");
 
 // GET all users
-router.get('/', async (req, res) => {
-    const allUsers = await User.find()
-    res.status(200).json(allUsers)
+router.get("/", async (req, res) => {
+  const allUsers = await User.find();
+  res.status(200).json(allUsers);
 });
 
 // GET a single user by its _id and populated thought and friend data
+router.get("/:userId", async (req, res) => {
+  const oneUser = await User.findById(req.params.userId);
+  res.status(200).json(oneUser);
+});
 
 // POST a new user:
 
